407f81cd96d5466fceae6f1080d06025d1d6472c
Leonard Lee <lee3266@data.cs.purdue.edu>
2022-01-23 19:52:08 -0500

    Done!

--- answers.sh
+++ answers.sh
@@ -4,42 +4,46 @@
 cd ORIGIN
 
 # Task 1 
-
+mv ORIGIN/Homework/MA165/HW2.txt ORIGIN/Homework/MA165/HW1.txt
 
 # Task 2
-
+rm ORIGIN/Memes/SpongeBob/money.jpeg
 
 # Task 3
-
+mv ORIGIN/Memes/'Star Wars'/fafsa.jpg ORIGIN/Memes/SpongeBob/fafsa.jpg
 
 # Task 4
-
+rm -r ORIGIN/Homework/CS191
 
 # Task 5
-
+mkdir ORIGIN/Programs/C++
 
 # Task 6
-
+mv ORIGIN/Programs/Age.cpp ORIGIN/Programs/C++/Age.cpp
 
 # Task 7
-
+cp -r ORIGIN/Programs/Java ~/homework-1-leo3266/ORIGIN/Homework/CS180/Project_1
 
 # Task 8
-
+mv ORIGIN/Programs/Count.c ORIGIN/Programs/Goodbye.c ORIGIN/Programs/C
 
 # Task 9
-
+mv ORIGIN/Programs/Name.py ORIGIN/Programs/Space.py ORIGIN/Programs/Python\ 3/
 
 # Task 10
-
+touch ORIGIN/Homework/POL141/Essay.txt
 
 # Task 11
-
+pwd 
+# /homes/lee3266/homework-1-leo3266 was printed 
+# did not fully understand the task 11, unsure what it means by current working directory.
 
 # Task 12
 : '
 Type your vim/nano customizations below:
+set autoread
+set number
+set mouse=a
 
 
 
-'
